951ebac16d6fc4c0c2559e3e024396ff8dbbd035
Gunaratna <VGUNADH@PEROOT.COM>
2016-10-02 13:09:37 +0530

    Food Model get details

--- app/Food.php
+++ app/Food.php
@@ -1,9 +1,14 @@
 <?php namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use DB;
 class Food extends Model {
 
-	//
+    public static function getFoodDetails()
+    {
+        $foods=DB::table('foods')
+            ->get();
+        return $foods;
+    }
 
 }
